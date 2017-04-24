5b0094183d9efaf5b7f89ead35e9075e31af96dc
Jack Grahl <jack.grahl@gmail.com>
2017-04-24 21:20:58 +0000

    Some code for testing the actual library, not the example.

---
+++ simple.py
@@ -0,0 +1,2 @@
+def add_2(n):
+    return n + 2
---
+++ test/test_mutate.py
@@ -0,0 +1,17 @@
+import unittest
+
+import ast
+
+import mutate
+
+with open("simple.py") as f:
+    simple_tree = ast.parse(f.read())
+   
+class TestTreeCopyAndModify(unittest.TestCase):
+    def test_simple_copy(self):
+        pass
+
+class TestTreeCopyIterate(unittest.TestCase):
+    def test_basic_iteration(self):
+        tmi = mutate.TreeMutationIterator(simple_tree)
+        self.assertIsNotNone(tmi)
